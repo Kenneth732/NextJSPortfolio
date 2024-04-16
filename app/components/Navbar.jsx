967fbcc535a8a73b0f52c0e88398357de6b5da14
KENNETH <kenneth.mburu@student.moringaschool.com>
2024-04-16 23:33:56 +0300

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,29 +1,29 @@
-
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
-      <header class="text-slate-700 container relative mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center">
-      <a href="#" class="flex items-center whitespace-nowrap text-2xl font-black">
-        <span class="mr-2 w-8">
-          <img className="logo-size" src="https://logowik.com/content/uploads/images/nextjs7685.logowik.com.webp" alt="" />
+      <header className="text-slate-700 container relative mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center">
+      <a href="#" className="flex items-center whitespace-nowrap text-2xl font-black">
+        <span className="mr-2 w-8">
+          <Image className="logo-size" src="https://logowik.com/content/uploads/images/nextjs7685.logowik.com.webp" alt="" width={32} height={32} />
         </span>
         
       </a>
-      <input type="checkbox" class="peer hidden" id="navbar-open" />
-      <label class="absolute top-5 right-5 cursor-pointer lg:hidden" for="navbar-open">
-        <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 6h16M4 12h16M4 18h16"></path>
+      <input type="checkbox" className="peer hidden" id="navbar-open" />
+      <label className="absolute top-5 right-5 cursor-pointer lg:hidden" htmlFor="navbar-open">
+        <svg className="h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 6h16M4 12h16M4 18h16"></path>
         </svg>
       </label>
-      <nav aria-label="Header Navigation" class="peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
-        <ul class="flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0">
-          <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Home</a></li>
-          <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">About</a></li>
-          <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Portfolio</a></li>
-          <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Contact</a></li>
-          <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">FAQ</a></li>
+      <nav aria-label="Header Navigation" className="peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
+        <ul className="flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0">
+          <li className="lg:mr-12"><a className="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Home</a></li>
+          <li className="lg:mr-12"><a className="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">About</a></li>
+          <li className="lg:mr-12"><a className="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Portfolio</a></li>
+          <li className="lg:mr-12"><a className="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Contact</a></li>
+          <li className="lg:mr-12"><a className="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">FAQ</a></li>
         </ul>
-        <hr class="mt-4 w-full lg:hidden" />
+        <hr className="mt-4 w-full lg:hidden" />
 
       </nav>
     </header>
